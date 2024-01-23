f9425862a599982c62a91d241abf1f072eb0d7bb
Matteo Falco <matteo.falco1986@gmail.com>
2024-01-23 11:18:54 +0000

    Add get experience

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import { Experience } from "./components/Experience";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      {/* La navbar va qua */}
-      <Routes>
-       {/*  qua dentro i vari route */}
+        {/* La navbar va qua */}
+        <Routes>
+          {/*  qua dentro i vari route */}
+          <Route path="/" element={<Experience />} />
       </Routes>
       </BrowserRouter>
     </>
