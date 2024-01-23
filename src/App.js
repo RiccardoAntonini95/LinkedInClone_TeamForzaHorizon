fbef9792978c98cd088a3602ef6872f1b9987446
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-01-23 17:18:42 +0100

    jobsearch component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Routes>
        <Route path="/" element = {<Home />} />
        <Route path="/profile" element = {<ProfilePage />} />
-       <Route path="/jobs" element = {<Jobs />} />
+       {/* <Route path="/jobs" element = {<Jobs />} /> */}
       </Routes>
       </BrowserRouter>
     </>
