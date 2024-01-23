fd29515a842eb66a6253ce4fec428e1816b72cde
Franncesco Caputo <caputo.francesco001@gmail.com>
2024-01-23 09:06:56 +0100

    createNav

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,1 +1,9 @@
-//commento es
+import React from "react";
+
+export default function Navbar() {
+  <div className="navbar">
+    <div className="navbar-wrap">
+      <h1>Navbar</h1>
+    </div>
+  </div>;
+}
