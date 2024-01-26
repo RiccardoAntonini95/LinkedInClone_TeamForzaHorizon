7229bff4f128409e45d12ac390b0a05231cf48e1
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-01-26 10:42:07 +0100

    fix nav develop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,13 @@ import PostBar from "./PostBar";
 import LeftSide from "./LeftSide";
 import { Row, Col } from "react-bootstrap";
 import PostList from "./PostList";
+import HomeFooter from "./HomeFooter";
 import { setProfileAction } from "../redux/actions/ProfilePage";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { STRIVE_KEY_MERLINO, STRIVE_KEY_COMMENTS} from "../assets/js/auth_keys";
+import { BsFillInfoSquareFill } from "react-icons/bs";
+import { IoIosArrowDown } from "react-icons/io";
 
 
 // IN CASO DI API MALFUNZIONANTE
@@ -41,12 +44,12 @@ const Home = () => {
   const [comments, setComments] = useState(null)
 /*   const [arrayComb, setArrayComb] = useState(null) */
 
-  /* const combinaArray = (newestData) => {
+/*   const combinaArray = (newestData) => {
     const arrayCombinato = []
     for (let i = 0; i < posts.length; i++) {
       let post = newestData[i];
       let commentArr = [];
-      for (let j = i; j < comments.length; j++) {
+      for (let j = 0; j < comments.length; j++) {
         if (posts._id === comments._id) {
           commentArr.push(comments[j]);
         }
@@ -60,8 +63,8 @@ const Home = () => {
     setArrayComb(arrayCombinato)
     console.log(arrayCombinato, "array risultato")
   
-  }
- */
+  } */
+
 
   useEffect(() => {
     getProfileData();
@@ -135,6 +138,45 @@ const Home = () => {
           <PostList key={i} posts={post} comments={comments}/>
         ))
         )}
+      </Col>
+      <Col xs={3} className="my-4">
+            <Row className="rounded">
+              <div className="rounded bg-white pt-3">
+                <Row>
+                  <Col><p className="fw-bold mb-1">LinkedIn News</p></Col>
+                  <Col className="text-end"><p><BsFillInfoSquareFill /></p></Col>            
+                </Row> 
+                <ul>
+                  <li>
+                    <p className="fw-bold m-0">Scientists Discover That Coffee is the Real Source of Renewable Energy!</p>
+                    <p className="text-secondary mb-1">Main news &middot; 331 readers</p>
+                  </li>
+                  <li>
+                    <p className="fw-bold m-0">Local Cat Declares War on Vacuum Cleaner: Demands Peace Talks</p>
+                    <p className="text-secondary mb-1">1 day ago</p>
+                  </li>
+                  <li>
+                    <p className="fw-bold m-0">Breaking News: World's Slowest Snail Sets New Speed Record</p>
+                    <p className="text-secondary mb-1">4 days ago</p>
+                  </li>
+                  <li>
+                    <p className="fw-bold m-0">Aliens Abduct Entire Pizza Delivery Fleet - Demands Earth's Best Recipes</p>
+                    <p className="text-secondary mb-1">19 hours ago</p>
+                  </li>
+                  <li>
+                    <p className="fw-bold m-0">Exclusive Interview with Sock: 'Where Do All the Missing Pairs Go?</p>
+                    <p className="text-secondary mb-1">2 days ago</p>
+                  </li>
+                </ul>
+                <p className="text-secondary">Show more <IoIosArrowDown /></p>
+              </div>
+              <img
+                src="https://media.licdn.com/media/AAYQAgTPAAgAAQAAAAAAADVuOvKzTF-3RD6j-qFPqhubBQ.png"
+                alt="See who's hiring on Linkedin"
+                className="ad-image my-3 p-0"
+              />
+              <HomeFooter />
+            </Row>
       </Col>
     </Row>
   );
